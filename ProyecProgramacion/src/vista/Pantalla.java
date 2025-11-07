56a9509c1475df2f23c83eb54fdc9d9e09211054
mmerchan211 <mmerchan889@unab.edu.co>
2025-11-06 22:01:08 -0500

    Update Pantalla.java

--- ProyecProgramacion/src/vista/Pantalla.java
+++ ProyecProgramacion/src/vista/Pantalla.java
@@ -1,8 +1,26 @@
 package vista;
+import javax.swing*;
+import java.awt*;
 
 public class Pantalla {
 
-    public void mostrarDato(double valor) {
-        System.out.println("ECG → Lectura actual: " + valor + " mV");
+private JLabel etiqueta;
+
+    public VentanaECG() {
+        setTitle("Monitor ECG");
+        setSize(300, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        etiqueta = new JLabel("ECG: ---", SwingConstants.CENTER);
+        etiqueta.setFont(new Font("Arial", Font.BOLD, 22));
+
+        add(etiqueta, BorderLayout.CENTER);
+
+        setVisible(true);
+    }
+
+    public void actualizarLectura(double valor) {
+        etiqueta.setText("ECG: " + valor + " mV");
     }
 }

5ec061e5adc551710086fc512fc5652c07e286ae
mmerchan211 <mmerchan889@unab.edu.co>
2025-11-06 22:04:10 -0500

    Update Main.java

--- ProyecProgramacion/src/app/Main.java
+++ ProyecProgramacion/src/app/Main.java
@@ -1,6 +1,9 @@
 package app;
 
-import modelo.BaseDatos;
+import javax.swing.JOptionPane;
+
+
+import modelo.ArchivoDatos;
 import modelo.Sensor;
 import vista.Pantalla;
 import controlador.MonitorECG;
@@ -8,11 +11,33 @@ import controlador.MonitorECG;
 public class Main {
     public static void main(String[] args) {
 
-        BaseDatos bd = new BaseDatos();
-        Sensor sensor = new Sensor(bd);
-        Pantalla pantalla = new Pantalla();
-        MonitorECG monitor = new MonitorECG(sensor, pantalla);
+      try {
 
-        monitor.iniciar(20);
+            String ruta = JOptionPane.showInputDialog(
+                    null,
+                    "Ingrese la ruta del archivo de datos (ej: ecg_data.txt):",
+                    "Fuente de Datos",
+                    JOptionPane.QUESTION_MESSAGE
+            );
+
+            if (ruta == null || ruta.trim().isEmpty()) {
+                throw new Exception("Debe ingresar una ruta válida.");
+            }
+
+            ArchivoDatos archivo = new ArchivoDatos(ruta);
+            Sensor sensor = new Sensor(archivo);
+            Pantalla Pantalla = new Pantalla();
+
+            MonitorECG monitor = new MonitorECG(sensor, ventana);
+            monitor.iniciar();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Error al iniciar el sistema:\n" + e.getMessage(),
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
     }
 }

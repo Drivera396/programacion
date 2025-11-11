62473d102351d82097872be135c95054704ec963
Drivera396 <drivera396@unab.edu.co>
2025-11-10 20:34:28 -0500

    Update Main.java

--- ProyecProgramacion/src/app/Main.java
+++ ProyecProgramacion/src/app/Main.java
@@ -2,42 +2,55 @@ package app;
 
 import javax.swing.JOptionPane;
 
-
-import modelo.ArchivoDatos;
-import modelo.Sensor;
-import vista.Pantalla;
-import controlador.MonitorECG;
-
 public class Main {
-    public static void main(String[] args) {
+    private static final String URL_DRIVE =
+            "https://drive.google.com/uc?export=download&id=1W2I6XviLH90jphh6gyN9uEHpHN60nRP7";
 
+ public static void main(String[] args) {   
       try {
+          ArchivoDatos datos = new ArchivoDatos(URL_DRIVE, true);
+          
+          String[] opciones = {
+                 "Ritmo sinusal normal",
+                 "Taquicardia ventricular",
+                 "Bradicardia sinusal"
+          };
 
-            String ruta = JOptionPane.showInputDialog(
+          int opc = JOptionPane.showOptionDialog(
                     null,
-                    "Ingrese la ruta del archivo de datos (ej: ecg_data.txt):",
-                    "Fuente de Datos",
-                    JOptionPane.QUESTION_MESSAGE
-            );
+                    "Seleccione el tipo de señal a graficar:",
+                    "MENÚ ECG",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    opciones,
+                    opciones[0]
+          };
+     if (opc == -1) return;
 
-            if (ruta == null || ruta.trim().isEmpty()) {
-                throw new Exception("Debe ingresar una ruta válida.");
+     String tipo = "";
+     switch (opc) {
+            case 0: tipo = "Normal"; break;
+            case 1: tipo = "Taquicardia"; break;
+            case 2: tipo = "Bradicardia"; break;
             }
-
-            ArchivoDatos archivo = new ArchivoDatos(ruta);
-            Sensor sensor = new Sensor(archivo);
-            Pantalla Pantalla = new Pantalla();
-
-            MonitorECG monitor = new MonitorECG(sensor, ventana);
-            monitor.iniciar();
-
+     
+     Graficador.mostrarGrafica(
+                    datos.getTiempo(),
+                    datos.getColumna(tipo),
+                    "ECG - " + tipo,
+                    "Tiempo (s)",
+                    "Voltaje (mV)"
+         );
+     
         } catch (Exception e) {
             JOptionPane.showMessageDialog(
                     null,
-                    "Error al iniciar el sistema:\n" + e.getMessage(),
+                    "Error cargando datos desde Drive:\n" + e.getMessage(),
                     "Error",
                     JOptionPane.ERROR_MESSAGE
             );
         }
     }
 }
+
